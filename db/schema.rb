2534ba509b3b1e2760b974037b7d8661518c16a4
github username <diep.christopher@gmail.com>
2018-07-09 16:02:00 -0400

    errors.full_messages said that meal_day_id was required, so migration created to have meal_day_id attr for days

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_190256) do
+ActiveRecord::Schema.define(version: 2018_07_09_195845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_07_03_190256) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "meal_day_id"
   end
 
   create_table "meal_days", force: :cascade do |t|
