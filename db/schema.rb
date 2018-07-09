72c1a4b13d438300f5631bb31ef15a975f7c66c3
github username <diep.christopher@gmail.com>
2018-07-09 15:04:46 -0400

    updated schema with meal days join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_185322) do
+ActiveRecord::Schema.define(version: 2018_07_03_190256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_07_03_185322) do
     t.integer "month"
     t.integer "day"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meal_days", force: :cascade do |t|
+    t.integer "meal_id"
+    t.integer "day_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
